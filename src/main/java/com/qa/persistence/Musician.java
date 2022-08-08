fa2990b568b88df6b83d40d2029d9aee0f5a6440
olly000 <ollystockman@gmail.com>
2022-08-08 13:21:51 +0100

    PP-5 #done #comment created musician entity

---
+++ src/main/java/com/qa/persistence/Musician.java
@@ -0,0 +1,32 @@
+package com.qa.persistence;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Musician {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	
+	@Column(nullable=false)
+	private String surname;
+	
+	@Column
+	private String forename;
+	
+	@Column
+	private String instrument;
+
+}
